4b1de7094b966d2d39b9a39a4f95df62750b51f0
Erik Pettersson <erikpettersson02@gmail.com>
2024-07-02 12:15:59 +0200

    Refined with less comments explaining simple tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,19 @@
 #include <avr/io.h>
 
-
-#define RED_LED_PIN 0 // Binary = 0b00 // Hexadecimal = 0x00 // Pin 8
-#define BUTTON_LED_PIN 1 // Binary = 0b01 // Hexadecimal = 0x01 // Pin 9
-#define SET_BIT(PORT, BIT) ((PORT) |= (1 << (BIT))) // Sätter en bit till 1 i ett port register / sätter pinnen till output / sätter den "high"
-#define CLEAR_BIT(PORT, BIT) ((PORT) &= ~(1 << (BIT))) // Sätter en bit till 0 i ett port register / sätter pinnen till input / sätter den "low"
-#define READ_BIT(PORT, BIT) (((PORT) & (1 << (BIT))) >> (BIT)) // Läser av värdet som en bit har i ett port register / läser av värdet på en input pinne / läser är den "low" eller "high"
+#define RED_LED_PIN 0 // Pin 8
+#define BUTTON_LED_PIN 1 // Pin 9
+#define SET_BIT(PORT, BIT) ((PORT) |= (1 << (BIT)))
+#define CLEAR_BIT(PORT, BIT) ((PORT) &= ~(1 << (BIT)))
+#define READ_BIT(PORT, BIT) (((PORT) & (1 << (BIT))) >> (BIT))
 
 bool gLedOn = false;
 bool gLastButtonPress = false;
 bool gButtonPressed = false;
 
 void setupGPIO(){
-  SET_BIT(DDRB, RED_LED_PIN); // Sätter led pinnen till output
-  CLEAR_BIT(DDRB, BUTTON_LED_PIN); // Sätter button pinnen till input
-  SET_BIT(PORTB, BUTTON_LED_PIN); // Aktiverar pull-up resistor för button pinnen
+  SET_BIT(DDRB, RED_LED_PIN); // setting the red led pin to output
+  CLEAR_BIT(DDRB, BUTTON_LED_PIN); // setting button pin to input
+  SET_BIT(PORTB, BUTTON_LED_PIN); // activate pull up resistors for button pinnen
 }
 
 bool isButtonPressed(){
@@ -52,18 +51,13 @@ void checkButtonPress(){
     setLedOnOrOff(gLedOn);
   }
   gLastButtonPress = gButtonPressed;
-
-}
-
-void loop() {
-  checkButtonPress();
 }
 
 int main(){
   setupGPIO();
   
   while(1){
-    loop();
+    checkButtonPress();
   }
   
   return 0;
